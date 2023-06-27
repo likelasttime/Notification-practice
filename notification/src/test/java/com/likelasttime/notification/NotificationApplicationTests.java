b8d7d653a964efb0e838336c4451559d22c0adf8
likelasttime <a50070863@gmail.com>
2023-06-27 10:44:55 +0900

    init

---
+++ notification/src/test/java/com/likelasttime/notification/NotificationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.likelasttime.notification;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
