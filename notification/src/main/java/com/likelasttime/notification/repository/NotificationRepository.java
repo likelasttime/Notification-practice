9a6ce4d519e43106e1791b9d2db3ff8c14a117bf
likelasttime <a50070863@gmail.com>
2023-06-28 12:07:21 +0900

    feat: 발송 가능한 알림 조회 (#15)

--- notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
+++ notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
@@ -4,6 +4,7 @@ package com.likelasttime.notification.repository;
 import com.likelasttime.notification.domain.Notification;
 import com.likelasttime.notification.domain.PushCase;
 import com.likelasttime.notification.domain.PushStatus;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
     Optional<Notification> findByPathIdAndPushStatusAndPushCase(
             Long pathId, PushStatus pushStatus, PushCase pushCase);
+
+    List<Notification> findByPushStatus(PushStatus pushStatus);
 }
