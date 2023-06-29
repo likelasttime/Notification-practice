78c234c4bbe0fdda8f0e87b6424308384afc23f2
likelasttime <a50070863@gmail.com>
2023-06-29 12:20:53 +0900

    feat: 발송이 완료된 알림 조회 로직 추가 (#18)

--- notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
+++ notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
@@ -6,6 +6,8 @@ import com.likelasttime.notification.domain.PushCase;
 import com.likelasttime.notification.domain.PushStatus;
 import java.util.List;
 import java.util.Optional;
+
+import com.likelasttime.notification.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -23,4 +25,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     void updatePushStatusIn(
             @Param("notifications") List<Notification> notifications,
             @Param("pushStatus") PushStatus pushStatus);
+
+    @Query("select pn from Notification pn "
+            + "where pn.user = :user and pn.pushStatus = :pushStatus "
+            + "order by pn.pushTime desc")
+    List<Notification> findAllLatestOrderByDesc(@Param("user") User user,
+                                                @Param("pushStatus") PushStatus pushStatus);
 }
