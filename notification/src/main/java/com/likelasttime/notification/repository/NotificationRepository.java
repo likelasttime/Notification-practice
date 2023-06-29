83141c0e4705cc2ab56c7c14ed6747c518a6b5c0
likelasttime <a50070863@gmail.com>
2023-06-29 11:18:32 +0900

    feat: 알림을 발송 완료 상태로 변경하는 로직 추가 (#17)

--- notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
+++ notification/src/main/java/com/likelasttime/notification/repository/NotificationRepository.java
@@ -7,6 +7,9 @@ import com.likelasttime.notification.domain.PushStatus;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
@@ -14,4 +17,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             Long pathId, PushStatus pushStatus, PushCase pushCase);
 
     List<Notification> findByPushStatus(PushStatus pushStatus);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update Notification pn set pn.pushStatus = :pushStatus where pn in :notifications")
+    void updatePushStatusIn(
+            @Param("notifications") List<Notification> notifications,
+            @Param("pushStatus") PushStatus pushStatus);
 }
